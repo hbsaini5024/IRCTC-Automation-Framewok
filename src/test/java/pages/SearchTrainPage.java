0c4fb4ab2e44023033b348980fd8b573d529e4b9
Harsh Bobby <62429141+hbsaini5024@users.noreply.github.com>
2025-10-05 11:42:17 +0530

    second commint

--- src/test/java/pages/SearchTrainPage.java
+++ src/test/java/pages/SearchTrainPage.java
@@ -16,8 +16,14 @@ public class SearchTrainPage {
 	By destinationInputField = By.xpath("//input[@role='searchbox']/ancestor::p-autocomplete[@id='destination']/descendant::input");
 	
 	By SearchBtn = By.xpath("//button[@label='Find Trains']");
-	
 	By numberOfTrains = By.xpath("//div[@class='hidden-xs']/span");
+	
+//	Filtering
+	By sortByDurationBtn = By.xpath("//button[text()=' Sort By ']");
+	By EarlyFirstBtn = By.xpath("//button[text()='Duration']/child::span[text()='Early First']");
+	By LateFirstBtn = By.xpath("//button[text()='Duration']/child::span[text()='Late First']");
+	By AcChairCarCheckBox = By.xpath("//input[@id='CC']/following-sibling::label");
+	By EcChairCarCheckBox = By.xpath("//input[@id='EC']/following-sibling::label");
 	
 	public void enterSearchDetails(String source, String destination) {
 		
@@ -35,6 +41,21 @@ public class SearchTrainPage {
 		System.out.println(trainCountStr);
 		
 		System.out.println("Total Trains are  : " + trainCountStr.split(" ")[0]);
+	}
+	
+	public void applyFilterByDuration() {
+		
+		driver.findElement(sortByDurationBtn).click();
+		
+		driver.findElement(LateFirstBtn).click();
+		
+	}
+	
+	public void applyFilterByCategory() {
+		
+		driver.findElement(AcChairCarCheckBox);
+		driver.findElement(EcChairCarCheckBox);
+		
 	}
 	
 
